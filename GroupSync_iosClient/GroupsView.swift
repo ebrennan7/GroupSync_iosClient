7ddb35aba2f931c2214f7c8ab75af4ce887127a1
14416022 <ember.brennan@ucdconnect.ie>
2017-12-04 17:30:23 +0000

    Navigation past the home page is working

---
+++ GroupSync_iosClient/GroupsView.swift
@@ -0,0 +1,36 @@
+//
+//  GroupsView.swift
+//  GroupSync_iosClient
+//
+//  Created by Ember Brennan on 04/12/2017.
+//  Copyright © 2017 EmberBrennan. All rights reserved.
+//
+
+import UIKit
+
+class GroupsView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GroupSync_iosClient/HomeView.swift
@@ -0,0 +1,44 @@
+//
+//  HomeView.swift
+//  GroupSync_iosClient
+//
+//  Created by Ember Brennan on 04/12/2017.
+//  Copyright © 2017 EmberBrennan. All rights reserved.
+//
+
+import UIKit
+
+class HomeView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    // Do any additional setup after loading the view.
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
