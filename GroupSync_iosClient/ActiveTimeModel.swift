eb22a6fabfcbeda18c47f751da190a5cb2bec1d1
ebrennan7 <XYZ_EMBER@HOTMAIL.COM>
2018-01-31 18:13:46 +0000

    Create Group Successful, fixed bug in GroupView

---
+++ GroupSync_iosClient/ActiveTimeModel.swift
@@ -0,0 +1,9 @@
+//
+//  ActiveTimeModel.swift
+//  GroupSync_iosClient
+//
+//  Created by Ember Brennan on 31/01/2018.
+//  Copyright © 2018 EmberBrennan. All rights reserved.
+//
+
+import Foundation
---
+++ GroupSync_iosClient/AlertModel.swift
@@ -0,0 +1,9 @@
+//
+//  AlertModel.swift
+//  GroupSync_iosClient
+//
+//  Created by Ember Brennan on 31/01/2018.
+//  Copyright © 2018 EmberBrennan. All rights reserved.
+//
+
+import Foundation
