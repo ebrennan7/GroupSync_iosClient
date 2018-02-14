981ace0046b4a45d7f79ee79338a5a78db470b1c
ebrennan7 <XYZ_EMBER@HOTMAIL.COM>
2018-02-14 17:44:17 +0000

    Added send locations

--- GroupSync_iosClient/GetUsers.swift
+++ GroupSync_iosClient/GetUsers.swift
@@ -7,3 +7,86 @@
 //
 
 import Foundation
+import CoreLocation
+
+
+class GetUsers{
+    
+    func getUsersForGroup(group_id: String, completionBlock: @escaping  ([(longitude: String, latitude: String)]) ->Void) -> Void
+    {
+        print(KeychainService.loadPassword()!)
+        print(group_id)
+    
+        
+        let headers = [
+            "Content-Type": "application/json",
+            "Cache-Control": "no-cache",
+            "Postman-Token": "55360735-c84c-8e11-d810-efbe9419040f"
+        ]
+        let parameters = [
+            "authToken": KeychainService.loadPassword()!,
+            "group_id": group_id
+            ] as [String : Any]
+        
+        let postData = try? JSONSerialization.data(withJSONObject: parameters, options: [])
+        guard postData != nil else{
+            return
+        }
+        let request = NSMutableURLRequest(url: NSURL(string: "http://groupsyncenv.rtimfc7um2.eu-west-1.elasticbeanstalk.com/get_groups_users")! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = headers
+        request.httpBody = postData
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                do{
+                    let resultJson = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments) as? [String:AnyObject]
+                    
+                    if let dictionary = resultJson{
+                        if let nestedDictionary = dictionary["data"] as? [String:Any]{
+                            if let locations = nestedDictionary["locations"] as? String
+                            {
+                                if let users = nestedDictionary["users"] as? String
+                                {
+                                let returnValue = self.parseLocations(locations: locations, users: users)
+                                    completionBlock(returnValue)
+                                }
+                            }
+                            
+                        }
+                        
+                    }
+                }
+                catch{
+                    print("Error -> \(error)")
+                }
+            }
+        })
+        
+        dataTask.resume()
+    }
+    
+    func parseLocations(locations: String, users: String) -> [(longitude: String, latitude: String)]{
+        
+        var splitLocations = locations.components(separatedBy: "{")
+        var stuff:[(longitude: String, latitude: String)] = []
+
+       
+        for f in stride(from: 1, to: splitLocations.count, by: 1)
+        {
+            stuff.append((longitude: (splitLocations[f].components(separatedBy: ":")[1].components(separatedBy: ",")[0]), latitude: (splitLocations[f].components(separatedBy: ":")[2].components(separatedBy: ",")[0])))
+
+        }
+     
+        return stuff
+      
+        
+   
+        
+    }
+}
--- GroupSync_iosClient/Map/MapViewController.swift
+++ GroupSync_iosClient/Map/MapViewController.swift
@@ -16,20 +16,22 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     var data: String! //group ID
     var annotation = MKPointAnnotation()
     var userLocation: CLLocation?
-
-
+    var locationTuples: [(longitude: String, latitude: String)]?
+    
+    
     @IBOutlet weak var mapView: MKMapView!
     
     var locationManager:CLLocationManager!
     
     let location = SendLocation()
-
+    
     
     @IBAction func mapSettingsButton(_ sender: UIButton) {
-    
-
         
-            self.location.determineCurrentLocation()
+        
+        
+        self.location.determineCurrentLocation()
+        
         
     }
     
@@ -39,38 +41,54 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         
         
         print(data)
+        let getUsers = GetUsers()
+        //Below calls the completion block and gets a 'return value' for getUsers//
+        (getUsers.getUsersForGroup(group_id: data) {(returnValue) in
+            self.locationTuples=returnValue
+        })
+        var f=0
+        
+        while(f<(locationTuples?.count)!)
+        {
+            
+            
+            annotation.coordinate = CLLocationCoordinate2D(latitude: Double(locationTuples![f].latitude)!, longitude: Double(locationTuples![f].longitude)!)
+            annotation.title = "User"
+            mapView.addAnnotation(annotation)
+            f=f+1
+        }
     }
     override func viewWillAppear(_ animated: Bool){
         super.viewWillAppear(animated)
         
-        determineCurrentLocation()
-     
+        //        determineCurrentLocation()
+        
     }
+    //
+    //    func determineCurrentLocation(){
+    //        locationManager = CLLocationManager()
+    //        locationManager.delegate = self
+    //        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    //        locationManager.requestAlwaysAuthorization()
+    //
+    //        if CLLocationManager.locationServicesEnabled(){
+    //            locationManager.startUpdatingLocation()
+    //        }
+    //    }
+    //
+    //    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+    //        userLocation = locations[0] as CLLocation
+    //
+    //
+    //        print("user latitude = \(userLocation?.coordinate.latitude)")
+    //        print("user longitude = \(userLocation?.coordinate.longitude)")
+    //        annotation.coordinate = CLLocationCoordinate2D(latitude: (userLocation?.coordinate.latitude)!, longitude: (userLocation?.coordinate.longitude)!)
+    //        annotation.title = "You"
+    //        mapView.addAnnotation(annotation)
+    //
+    //
+    //    }
     
-    func determineCurrentLocation(){
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestAlwaysAuthorization()
-
-        if CLLocationManager.locationServicesEnabled(){
-            locationManager.startUpdatingLocation()
-        }
-    }
-
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
-       userLocation = locations[0] as CLLocation
-
-
-        print("user latitude = \(userLocation?.coordinate.latitude)")
-        print("user longitude = \(userLocation?.coordinate.longitude)")
-        annotation.coordinate = CLLocationCoordinate2D(latitude: (userLocation?.coordinate.latitude)!, longitude: (userLocation?.coordinate.longitude)!)
-        annotation.title = "You"
-        mapView.addAnnotation(annotation)
-
-
-    }
-
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
     {
         print("Error \(error)")
@@ -80,15 +98,15 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
